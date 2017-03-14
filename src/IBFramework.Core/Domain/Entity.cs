3350c802f5ab878c0edaa7fb631a6da5e7a84c59
Ian Buchan <ibuchan72390@gmail.com>
2017-03-14 11:20:29 -0700

    Add some base classes that I can reuse in other projects

---
+++ src/IBFramework.Core/Domain/Entity.cs
@@ -0,0 +1,8 @@
+﻿using IBFramework.Core.Data.Domain;
+
+namespace IBFramework.Core.Domain
+{
+    public class Entity : EntityWithTypedId<int>, IEntity
+    {
+    }
+}
---
+++ src/IBFramework.Core/Domain/EntityWithTypedId.cs
@@ -0,0 +1,9 @@
+﻿using IBFramework.Core.Data.Domain;
+
+namespace IBFramework.Core.Domain
+{
+    public class EntityWithTypedId<T> : IEntityWithTypedId<T>
+    {
+        public T Id { get; set; }
+    }
+}
