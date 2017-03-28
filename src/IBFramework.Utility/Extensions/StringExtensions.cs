1c7acccbfa4f7f792c5ab740115ffa5d43a706aa
Ian Buchan <ibuchan72390@gmail.com>
2017-03-28 15:49:41 -0700

    Add string extension to generate an MD5 hash.  Seems this is used in some of our API work.

---
+++ src/IBFramework.Utility/Extensions/StringExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace IBFramework.Utility.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToMD5Hash(this string toConvert)
+        {
+            // byte array representation of that string
+            byte[] encodedPassword = new UTF8Encoding().GetBytes(toConvert);
+
+            // need MD5 to calculate the hash
+            var md5Algo = MD5.Create();
+            byte[] hash = md5Algo.ComputeHash(encodedPassword);
+
+            // string representation (similar to UNIX format)
+            string encoded = BitConverter.ToString(hash)
+               // without dashes
+               .Replace("-", string.Empty)
+               // make lowercase
+               .ToLower();
+
+            return encoded;
+        }
+    }
+}
