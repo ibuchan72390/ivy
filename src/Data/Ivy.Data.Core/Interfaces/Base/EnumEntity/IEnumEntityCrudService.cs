eef877b01e18ebdc0134d094383378fd9e8baee3
Ian Buchan <ibuchan72390@gmail.com>
2017-07-24 13:24:53 -0700

    Enforce a slightly more restrict interface requirement for the IEnumEntityCrudService

--- src/Data/Ivy.Data.Core/Interfaces/Base/EnumEntity/IEnumEntityCrudService.cs
+++ src/Data/Ivy.Data.Core/Interfaces/Base/EnumEntity/IEnumEntityCrudService.cs
@@ -6,10 +6,11 @@ namespace Ivy.Data.Core.Interfaces.Base.EnumEntity
 {
     public interface IEnumEntityCrudService<TEntity, TRepo, TEnum> : 
         IEnumEntityService<TEntity, TRepo, TEnum>,
-        IEntityCrudService<TEntity, TRepo>
+        IEntityCrudService<TEntity, TRepo>,
+        IGetByName<TEntity, TEnum>
 
         where TEntity : class, IEnumEntity<TEnum>
-        where TRepo : IEntityRepository<TEntity>
+        where TRepo : IEnumEntityRepository<TEntity, TEnum>
         where TEnum : struct, IComparable, IFormattable, IConvertible
     {
     }
