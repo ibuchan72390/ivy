911e20ea25ae7e07c414bd2565deafa320ed5349
Ian Buchan <ibuchan72390@gmail.com>
2017-09-12 09:51:20 -0700

    Fix a poor object instantiation.

--- src/Data/Ivy.Data.Common/Pagination/PaginationResponse.cs
+++ src/Data/Ivy.Data.Common/Pagination/PaginationResponse.cs
@@ -5,8 +5,21 @@ namespace Ivy.Data.Common.Pagination
 {
     public class PaginationResponse<T> : IPaginationResponse<T>
     {
+        #region Constructor
+
+        public PaginationResponse()
+        {
+            Data = new List<T>();
+        }
+
+        #endregion
+
+        #region Public Methods
+
         public IEnumerable<T> Data { get; set; }
 
         public int TotalCount { get; set; }
+
+        #endregion
     }
 }
