29666e70038adb27018a9e485c887d8f372b1b03
Ian Buchan <ibuchan72390@gmail.com>
2017-11-21 10:37:58 -0700

    A few more edits to enable some enhanced Mock testing.

--- src/Test/Ivy.TestUtilities/Base/GenericTestBase.cs
+++ src/Test/Ivy.TestUtilities/Base/GenericTestBase.cs
@@ -1,5 +1,6 @@
 ﻿using Ivy.IoC;
 using Ivy.IoC.Core;
+using Moq;
 using System;
 
 namespace Ivy.TestUtilities.Base
@@ -22,7 +23,8 @@ namespace Ivy.TestUtilities.Base
 
         public GenericTestBase()
         {
-            var containerGen = ServiceLocator.Instance.Resolve<IContainerGenerator>();
+            // Need to new up our container really quick
+            var containerGen = new ContainerGenerator();
 
             // Initialize our container
             InitializeContainerFn(containerGen);
@@ -40,6 +42,25 @@ namespace Ivy.TestUtilities.Base
         public void Dispose()
         {
             TearDownFn();
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        protected IMock<T> InitializeMoq<T>(IContainerGenerator containerGen)
+            where T : class
+        {
+            var mock = new Mock<T>();
+            containerGen.RegisterInstance<T>(mock);
+            return mock;
+        }
+
+        protected IContainerGenerator GetMockingContainerGenerator()
+        {
+            var containerGen = Container.Resolve<IContainerGenerator>();
+            InitializeContainerFn(containerGen);
+            return containerGen;
         }
 
         #endregion
