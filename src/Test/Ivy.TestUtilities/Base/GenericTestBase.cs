13284da814998dd55757d3b48de5811cee0bfe77
Ian Buchan <ibuchan72390@gmail.com>
2017-11-22 12:45:14 -0700

    Make a few further improvements on this test base.  I think this is getting closer to what it should actually be.

--- src/Test/Ivy.TestUtilities/Base/GenericTestBase.cs
+++ src/Test/Ivy.TestUtilities/Base/GenericTestBase.cs
@@ -1,7 +1,15 @@
 ﻿using Ivy.IoC;
 using Ivy.IoC.Core;
+using Ivy.IoC.IoC;
 using Moq;
 using System;
+
+/*
+ * Examine the references on this as it develops further...
+ * I don't think this is going to have an odd effect on test references at all;
+ * however, if this starts heavily expanding the libarary references, we may want
+ * to move this into it's own separate library for Ivy.TestHarness
+ */
 
 namespace Ivy.TestUtilities.Base
 {
@@ -18,7 +26,6 @@ namespace Ivy.TestUtilities.Base
 
         // Placeholders for custom test setup and teardown
         // Override the InitailizeContainerFn and invoke base.InitializeContainer(containerGen) to setup Mock tests
-        protected abstract void InitializeContainerFn(IContainerGenerator containerGen);
         protected abstract void TearDownFn();
 
         #endregion
@@ -47,9 +54,26 @@ namespace Ivy.TestUtilities.Base
             TearDownFn();
         }
 
+        protected virtual void InitializeContainerFn(IContainerGenerator containerGen)
+        {
+            // We resolve the ServiceLocator in Constructor
+            // May as well install this guy here while we're at it.
+            containerGen.InstallIvyIoC();
+        }
+
         #endregion
 
         #region Helper Methods
+
+        protected virtual void InitMockingContainer(Func<IContainerGenerator, IContainerGenerator> mockSetupFn)
+        {
+            var containerGen = new ContainerGenerator();
+            InitializeContainerFn(containerGen);
+
+            mockSetupFn(containerGen);
+
+            InitTestContainer(containerGen);
+        }
 
         protected virtual void InitTestContainer(IContainerGenerator containerGen = null)
         {
@@ -65,13 +89,6 @@ namespace Ivy.TestUtilities.Base
             var mock = new Mock<T>();
             containerGen.RegisterInstance<T>(mock.Object);
             return mock;
-        }
-
-        protected IContainerGenerator GetMockingContainerGen()
-        {
-            var containerGen = new ContainerGenerator();
-            InitializeContainerFn(containerGen);
-            return containerGen;
         }
 
         #endregion
