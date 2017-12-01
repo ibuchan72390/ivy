31b0025286d35eafdb9ba3046e76974ed29e0e08
Ian Buchan <ibuchan72390@gmail.com>
2017-12-01 16:18:09 -0700

    Add some extra extensions to simplify working with Enum values.

---
+++ src/Utility/Ivy.Utility.Core/Extensions/EnumExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Ivy.Data.Core.Interfaces.Domain;
+using System;
+
+namespace Ivy.Utility.Core.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static TEnumEntity ToEnumEntityWithTypedId<TEnum, TEnumEntity, TId>(this TEnum enumVal)
+            where TEnum : struct, IComparable, IFormattable, IConvertible
+            where TEnumEntity : IEnumEntityWithTypedId<TId, TEnum>, new()
+        {
+            return new TEnumEntity
+            {
+                Name = enumVal.ToString()
+            };
+        }
+
+        public static TEnumEntity ToEnumEntity<TEnum, TEnumEntity>(this TEnum enumVal)
+            where TEnum : struct, IComparable, IFormattable, IConvertible
+            where TEnumEntity : IEnumEntity<TEnum>, new()
+        {
+            return enumVal.ToEnumEntityWithTypedId<TEnum, TEnumEntity, int>();
+        }
+    }
+}
---
+++ src/Utility/Ivy.Utility.Test/Extensions/EnumExtensionsTests.cs
@@ -0,0 +1,67 @@
+﻿using Ivy.Data.Core.Domain;
+using Ivy.TestHelper;
+using Ivy.Utility.Core.Extensions;
+using Ivy.Utility.Core.Util;
+using Xunit;
+
+namespace Ivy.Utility.Test.Extensions
+{
+    public class EnumExtensionsTests : TestBase
+    {
+        #region Test Classes
+
+        private enum TestOtherEnum
+        {
+            TestOtherVal1
+        }
+
+        private enum TestEnum
+        {
+            TestVal1,
+            TestVal2,
+            TestVal3
+        }
+
+        private class TestEnumEntity : EnumEntity<TestEnum>
+        {
+        }
+
+        private class TestEnumEntityWithTypedId : EnumEntityWithTypedId<string, TestEnum>
+        {
+        }
+
+        #endregion
+
+        #region Tests
+
+        #region ToEnumEntityWithTypedId
+
+        [Fact]
+        public void ToEnumEntityWithTypedId_Works_As_Expected()
+        {
+            foreach (var val in EnumUtility.GetValues<TestEnum>())
+            {
+                var result = val.ToEnumEntityWithTypedId<TestEnum, TestEnumEntityWithTypedId, string>();
+                Assert.Equal(val.ToString(), result.Name);
+            }
+        }
+
+        #endregion
+
+        #region ToEnumEntity
+
+        [Fact]
+        public void ToEnumEntity_Works_As_Expected()
+        {
+            foreach (var val in EnumUtility.GetValues<TestEnum>())
+            {
+                var result = val.ToEnumEntity<TestEnum, TestEnumEntity>();
+                Assert.Equal(val.ToString(), result.Name);
+            }
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
