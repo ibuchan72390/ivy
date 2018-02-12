ac0ed0fbd307ec68867ddabeb0724ffe0a21aa2c
Ian Buchan <ibuchan72390@gmail.com>
2018-02-12 10:38:13 -0700

    Update the project versions accordingly to account for the previous interface changes we made

--- src/Data/Ivy.Data.Core/Interfaces/Base/Entity/IEntityCrudService.cs
+++ src/Data/Ivy.Data.Core/Interfaces/Base/Entity/IEntityCrudService.cs
@@ -4,7 +4,22 @@ using Ivy.Data.Core.Interfaces.Functional.Entity;
 
 namespace Ivy.Data.Core.Interfaces.Base.Entity
 {
+    public interface IEntityCrudService<TEntity, TKey, TRepo> :
+        IEntityService<TEntity, TKey, TRepo>,
+        IGetAll<TEntity>,
+        IPaginatedGetAll<TEntity>,
+        IDeleteAll<TEntity>,
+        IEntityDelete<TEntity, TKey>,
+        IEntityGet<TEntity, TKey>,
+        IEntitySaveOrUpdate<TEntity, TKey>
+
+        where TEntity : class, IEntityWithTypedId<TKey>
+        where TRepo : IEntityRepository<TEntity, TKey>
+    {
+    }
+
     public interface IEntityCrudService<TEntity, TRepo> : 
+        IEntityCrudService<TEntity, int, TRepo>,
         IEntityService<TEntity, TRepo>,
         IGetAll<TEntity>,
         IPaginatedGetAll<TEntity>,
--- src/Data/Ivy.Data.Core/Interfaces/Base/Entity/IEntityService.cs
+++ src/Data/Ivy.Data.Core/Interfaces/Base/Entity/IEntityService.cs
@@ -3,7 +3,13 @@ using Ivy.Data.Core.Interfaces.Init;
 
 namespace Ivy.Data.Core.Interfaces.Base.Entity
 {
-    public interface IEntityService<TEntity, TRepo> : IInitialize
+    public interface IEntityService<TEntity, TKey, TRepo> : IInitialize
+        where TEntity : class, IEntityWithTypedId<TKey>
+        where TRepo : IEntityRepository<TEntity, TKey>
+    {
+    }
+
+    public interface IEntityService<TEntity, TRepo> : IInitialize, IEntityService<TEntity, int, TRepo>
         where TEntity : class, IEntity
         where TRepo : IEntityRepository<TEntity>
     {
