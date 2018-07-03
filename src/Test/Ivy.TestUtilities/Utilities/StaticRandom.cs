db08e549a29c7cd0ddbc93bde2057e0e107d7029
Ian Buchan <ibuchan72390@gmail.com>
2018-07-03 11:41:19 -0700

    Add new class to the TestUtilities library.  This will help us work with IRandomizationHelper without having to worry about the Mocking context.

---
+++ src/Test/Ivy.TestUtilities/Utilities/StaticRandom.cs
@@ -0,0 +1,36 @@
+﻿using Ivy.IoC;
+using Ivy.Utility.Core;
+using Ivy.Utility.IoC;
+
+/*
+ * When working in a Mocking context, 
+ * this will allow you to access the IRandomizationHelper 
+ * functionality without going through a modified container.
+ * 
+ * This ensures you will never pull a Mock<IRandomizationHelper> on accident
+ */
+namespace Ivy.TestUtilities.Utilities
+{
+    public static class StaticRandom
+    {
+        private static IRandomizationHelper _rand;
+
+        private static IRandomizationHelper Rand
+        {
+            get {
+
+                if (_rand == null)
+                {
+                    var containerGen = new ContainerGenerator();
+
+                    containerGen.InstallIvyUtility();
+
+                    _rand = containerGen.GenerateContainer()
+                        .GetService<IRandomizationHelper>();
+                }
+
+                return _rand;
+            }
+        }
+    }
+}
