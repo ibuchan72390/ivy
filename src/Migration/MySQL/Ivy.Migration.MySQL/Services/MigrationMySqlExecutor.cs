3a9c70cad741080b08a74ccaeac9c5137f0dc36d
Ian Buchan <ibuchan72390@gmail.com>
2018-07-09 11:44:38 -0700

    Seems we're having some crazy issues on CI leveraging some transactions.  I'm curious if it has something to do with this Executor we've built.  I can get certain executions to work on Linux, but it seems that multiple in parallel are not working.  I'm concerned that we're somehow failing to properly end the transaction causing the next script to fail because of a missing table reference.

--- src/Migration/MySQL/Ivy.Migration.MySQL/Services/MigrationMySqlExecutor.cs
+++ src/Migration/MySQL/Ivy.Migration.MySQL/Services/MigrationMySqlExecutor.cs
@@ -8,7 +8,7 @@ namespace Ivy.Migration.MySQL.Services
     {
         #region Variables & Constants
 
-        private readonly ITransactionHelper _tranHelper;
+        private readonly ITranConnGenerator _tcGen;
 
         private readonly IMySqlScriptBuilder _scriptBuilder;
         private readonly IMySqlScriptExecutor _scriptExecutor;
@@ -18,11 +18,11 @@ namespace Ivy.Migration.MySQL.Services
         #region Constructor
 
         public MigrationMySqlExecutor(
-            ITransactionHelper tranHelper,
+            ITranConnGenerator tcGen,
             IMySqlScriptBuilder scriptBuilder,
             IMySqlScriptExecutor scriptExecutor)
         {
-            _tranHelper = tranHelper;
+            _tcGen = tcGen;
             _scriptBuilder = scriptBuilder;
             _scriptExecutor = scriptExecutor;
         }
@@ -33,14 +33,15 @@ namespace Ivy.Migration.MySQL.Services
 
         public void ExecuteSql(string scriptText, string connectionString)
         {
-            _tranHelper.InitializeByConnectionString(connectionString);
-
-            _tranHelper.WrapInTransaction(tran => {
-
-                var script = _scriptBuilder.GenerateScript(tran, scriptText);
+            using (ITranConn tc = _tcGen.GenerateTranConn(connectionString))
+            {
+                var script = _scriptBuilder.GenerateScript(tc, scriptText);
 
                 _scriptExecutor.Execute(script);
-            });
+
+                tc.Transaction.Commit();
+                tc.Connection.Close();
+            }
         }
 
         #endregion
