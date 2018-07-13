6b40415152e27cc09c03748b7c89abf1914a3858
Ian Buchan <ibuchan72390@gmail.com>
2018-07-13 09:29:06 -0700

    Enhance the data interfaces accordingly.

--- src/Data/Ivy.Data.Core/Interfaces/Functional/Entity/IEntitySaveOrUpdate.cs
+++ src/Data/Ivy.Data.Core/Interfaces/Functional/Entity/IEntitySaveOrUpdate.cs
@@ -3,12 +3,23 @@ using System.Collections.Generic;
 
 namespace Ivy.Data.Core.Interfaces.Functional.Entity
 {
-    public interface IEntitySaveOrUpdate<TEntity, TKey> 
-        where TEntity : class, IEntityWithTypedId<TKey>
+    public interface ISaveOrUpdateEntity<TEntity, TKey>
+        where TEntity: class, IEntityWithTypedId<TKey>
     {
         TEntity SaveOrUpdate(TEntity entity, ITranConn tc = null);
+    }
 
+    public interface ISaveOrUpdateEntities<TEntity, TKey>
+        where TEntity : class, IEntityWithTypedId<TKey>
+    {
         IEnumerable<TEntity> SaveOrUpdate(IEnumerable<TEntity> entities, ITranConn tc = null);
+    }
+
+    public interface IEntitySaveOrUpdate<TEntity, TKey> : 
+        ISaveOrUpdateEntity<TEntity, TKey>,
+        ISaveOrUpdateEntities<TEntity, TKey>
+        where TEntity : class, IEntityWithTypedId<TKey>
+    {
     }
 
     public interface IEntitySaveOrUpdate<TEntity> : IEntitySaveOrUpdate<TEntity, int>
